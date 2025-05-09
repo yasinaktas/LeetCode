3344b3db53198d02aa21f5981a71d017ad0e174c
Yasin Aktaş <100588947+yasinaktas@users.noreply.github.com>
2025-05-09 14:49:40 +0300

    Time: 0 ms (100.00%) | Memory: 40.9 MB (90.10%) - LeetSync

---
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.kt
@@ -0,0 +1,25 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun inorderTraversal(root: TreeNode?): List<Int> {
+        val result = mutableListOf<Int>()
+    
+        fun traverse(node: TreeNode?) {
+            if (node == null) return
+            traverse(node.left)
+            result.add(node.`val`)
+            traverse(node.right)
+        }
+
+        traverse(root)
+        return result
+    }
+}
